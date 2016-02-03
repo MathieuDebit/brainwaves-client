3d7b4d9efd89331676026715ab6f1cc151fde19f
Mathieu Débit <mathieu.debit@gmail.com>
2016-02-03 05:28:59 +0100

    Update `Login` component : setup authentication

--- source/javascripts/components/Login.jsx
+++ source/javascripts/components/Login.jsx
@@ -4,8 +4,40 @@ class Login extends React.Component {
   render() {
     return (
       <div>
-        <h2>Login</h2>
+        <h2>Log in</h2>
+
+        <form id='log-in'>
+          <input className='form-email' type='email' name='email'
+                 placeholder='Email' />
+
+          <input className='form-password' type='password' name='password'
+                 placeholder='Password' />
+
+               <button onClick={this.handleLoginClick} disabled={false}>
+            Log in
+          </button>
+        </form>
       </div>
     );
+  }
+
+  handleLoginClick(e) {
+    e.preventDefault();
+
+    var email = $('#log-in .form-email').val();
+    var password = $('#log-in .form-password').val();
+
+    $.auth.emailSignIn({email, password}, {
+      email: email,
+      password: password,
+      config: 'default'
+
+    }).then(function(resp) {
+      console.log(resp);
+      browserHistory.push('/');
+
+    }).fail(function(resp) {
+      console.log(resp);
+    });
   }
 }
